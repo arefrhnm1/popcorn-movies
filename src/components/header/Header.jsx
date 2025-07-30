7e9b15ad1bbf2f499f03e1feaa71c8c0bae1e4b8
Aref Rahnama <arefrhnm1@gmail.com>
2025-07-29 18:26:58 -0700

    feat(ui): added 'Follow Us' section with basic social media links

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -1,12 +1,14 @@
 import React from 'react'
 import Navigation from './Navigation'
 import SearchBox from './SearchBox'
+import FollowUs from './FollowUs'
 
 export default function Header() {
   return (
     <header>
         <Navigation/>
         <SearchBox/>
+        <FollowUs/>
     </header>
   )
 }
